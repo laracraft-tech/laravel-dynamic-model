623c9a59e4c34e39cada8b46c4ef5c6c310f59ba
skop <skopflores@gmail.com>
2026-01-21 16:45:37 -0600

    make DynamicModelFactory::create() nullable-safe for PHP 8.1+

--- src/DynamicModelFactory.php
+++ src/DynamicModelFactory.php
@@ -18,7 +18,7 @@ class DynamicModelFactory
     /**
      * @throws ReflectionException
      */
-    public function create(string $fqnClass, string $dynamicTableName, string $dynamicConnectionName = null): Model&DynamicModelInterface
+    public function create(string $fqnClass, string $dynamicTableName, string|null $dynamicConnectionName = null): Model&DynamicModelInterface
     {
         $this->dynamicTableName = $dynamicTableName;
         $this->dynamicConnectionName = $dynamicConnectionName ?? config('database.default');
